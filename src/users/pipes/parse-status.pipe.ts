9a6d22d439d09f6f4e2b179646e4a7227a2a9666
Luis Moreirac <lmoreirat@uteq.edu.ec>
2024-01-13 20:56:52 -0500

    corrections

--- src/users/pipes/parse-status.pipe.ts
+++ src/users/pipes/parse-status.pipe.ts
@@ -1,17 +1,19 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
 
 @Injectable()
-export class ParseStatusPipe implements PipeTransform<string, boolean> {
-    transform(value: string, metadata: ArgumentMetadata): boolean {
-        if (value === undefined) {
-            // Valor por defecto cuando no se proporciona un valor
-            return undefined; // Puedes cambiar el valor por defecto según tus necesidades
-        }
-        // El pipe intentará analizar el valor como booleano
-        const parsedValue = value.toLowerCase() === 'true';
-        if (parsedValue === undefined) {
-            throw new BadRequestException('Invalid boolean string');
-        }
-        return parsedValue;
+export class ParseStatusPipe
+  implements PipeTransform<string, boolean | string>
+{
+  transform(value: string, metadata: ArgumentMetadata): boolean | string {
+    if (value === undefined) {
+      // Valor por defecto cuando no se proporciona un valor
+      throw new BadRequestException('Invalid boolean string'); // Puedes cambiar el valor por defecto según tus necesidades
     }
+    return value;
+  }
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,11 +1,15 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from '@/users/dtos/UsersDtos';
 import { PrismaService } from '@/prisma.service';
 import { hashSync } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private db: PrismaService) { }
+  constructor(private db: PrismaService) {}
 
   async create(data: CreateUserDto) {
     const user = await this.db.user.findFirst({
@@ -16,40 +20,60 @@ export class UsersService {
       throw new BadRequestException('El usuario ya existe');
     }
 
-    data.password = hashSync(data.password, 10)
-    await this.db.user.create({ data }).catch((error) => { console.log(error); throw new BadRequestException(`Error al crear el usuario`); });
+    data.password = hashSync(data.password, 10);
+    await this.db.user.create({ data }).catch((error) => {
+      throw new BadRequestException(`Error al crear el usuario`);
+    });
 
     return { message: 'Usuario creado correctamente' };
   }
 
   async getProfile(id: string) {
-    return await this.db.user.findUniqueOrThrow({
-      where: { id },
-      select: {
-        id: true,
-        email: true,
-        firstName: true,
-        lastName: true,
-        createdAt: true,
-        updatedAt: true,
-        birthDate: true,
-        gender: true,
-      },
-    }).catch((error) => { throw new NotFoundException(`Error al obtener el usuario`) });
+    return await this.db.user
+      .findUniqueOrThrow({
+        where: { id },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+          updatedAt: true,
+          birthDate: true,
+          gender: true,
+        },
+      })
+      .catch((error) => {
+        throw new NotFoundException(`Error al obtener el usuario`);
+      });
   }
 
   async updateProfile(id: string, data: UpdateUserDto) {
-    const user = await this.db.user.findUniqueOrThrow({ where: { id } }).catch((error) => { throw new NotFoundException(`Error al obtener el usuario`) });
+    const user = await this.db.user
+      .findUniqueOrThrow({ where: { id } })
+      .catch((error) => {
+        throw new NotFoundException(`Error al obtener el usuario`);
+      });
 
-    await this.db.user.update({ where: { id }, data }).catch((error) => { throw new BadRequestException(`Error al actualizar el usuario`) });
+    await this.db.user.update({ where: { id }, data }).catch((error) => {
+      throw new BadRequestException(`Error al actualizar el usuario`);
+    });
 
     return { message: 'Usuario actualizado correctamente' };
   }
 
   async updateStatus(id: string) {
-    const user = await this.db.user.findUniqueOrThrow({ where: { id } }).catch((error) => { throw new NotFoundException(`Error al obtener el usuario`) });
+    const user = await this.db.user
+      .findUniqueOrThrow({ where: { id } })
+      .catch((error) => {
+        throw new NotFoundException(`Error al obtener el usuario`);
+      });
 
-    await this.db.user.update({ where: { id }, data: { status: !user.status } }).catch((error) => { throw new BadRequestException(`Error al actualizar el usuario`) });
+    await this.db.user
+      .update({ where: { id }, data: { status: !user.status } })
+      .catch((error) => {
+        throw new BadRequestException(`Error al actualizar el usuario`);
+      });
 
     return { message: 'Usuario actualizado correctamente' };
   }
