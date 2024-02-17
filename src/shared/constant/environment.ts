f441c671880f8c8275b8146db7b71fdd79d1a2c9
Luis Moreirac <lmoreirat@uteq.edu.ec>
2024-02-17 13:12:25 -0500

    Actualizar clave de API de IA en el archivo de entorno

--- src/shared/constant/environment.ts
+++ src/shared/constant/environment.ts
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 export const ENVIRONMENT = {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'sociabot2024',
-  API_KEY_AI:
-    process.env.API_KEY_AI || 'AIzaSyD-9tSrZ8J5JL3J3z5Z3J5J3J5J3J5J3J5',
+  API_KEY_AI: process.env.API_KEY_AI || '',
   IA_MODEL: process.env.IA_MODEL || 'gemini-pro',
   MAX_OUTPUT_TOKENS: process.env.MAX_OUTPUT_TOKENS || 100,
 };
